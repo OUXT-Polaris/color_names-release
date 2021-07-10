3134230e6a53cb29cf02daa2265f70b494db4dd2
Masaya Kataoka <ms.kataoka@gmail.com>
2021-07-10 20:54:52 +0900

    Rebase from 'upstream'

--- src/color_names.cpp
+++ src/color_names.cpp
@@ -1,4 +1,28 @@
-#include <color_names/color_names.h>
+/*
+Copyright (c) 2019 shuhei yoshida
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+#include <color_names/color_names.hpp>
+#include <string>
 
 #include <iostream>
 
@@ -6,11 +30,11 @@ namespace color_names
 {
 /**
  * @brief generate std_msgs::msg::ColorRGBA message from hsv values
- * @param h hue 
- * @param s saturation 
+ * @param h hue
+ * @param s saturation
  * @param v value
  * @param alpha alpha value of the color
- * @return std_msgs::msg::ColorRGBA 
+ * @return std_msgs::msg::ColorRGBA
  */
 std_msgs::msg::ColorRGBA fromHsv(double h, double s, double v, double alpha)
 {
@@ -21,8 +45,8 @@ std_msgs::msg::ColorRGBA fromHsv(double h, double s, double v, double alpha)
   float b = v;
   if (s > 0.0f) {
     h *= 6.0f;
-    int i = (int)h;
-    float f = h - (float)i;
+    int i = static_cast<int>(h);
+    float f = h - static_cast<float>(i);
     switch (i) {
       default:
       case 0:
@@ -61,14 +85,14 @@ std_msgs::msg::ColorRGBA fromHsv(double h, double s, double v, double alpha)
  * @brief generate std_msgs::msg::ColorRGBA message from color name
  * @param preset_name the name of the color
  * @param alpha alpha value of the color
- * @return std_msgs::msg::ColorRGBA 
+ * @return std_msgs::msg::ColorRGBA
  */
 std_msgs::msg::ColorRGBA makeColorMsg(std::string preset_name, double alpha)
 {
   std_msgs::msg::ColorRGBA c_msg;
   c_msg.a = alpha;
-  if (c_msg.a < 0.) c_msg.a = 0.;
-  if (c_msg.a > 1.) c_msg.a = 1.;
+  if (c_msg.a < 0.) {c_msg.a = 0.;}
+  if (c_msg.a > 1.) {c_msg.a = 1.;}
 
   auto found_itr = COLOR_NAME_DICT.find(preset_name);
   if (found_itr != COLOR_NAME_DICT.end()) {
