044d42cf922ca0a116a19226c8be690d0a8614d0
MasayaKataoka <ms.kataoka@gmail.com>
2022-01-15 20:51:19 +0900

    Rebase from 'upstream'

--- src/color_names.cpp
+++ src/color_names.cpp
@@ -1,30 +1,20 @@
-/*
-Copyright (c) 2019 shuhei yoshida
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-*/
-
+// Copyright (c) 2020 OUXT Polaris
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #include <color_names/color_names.hpp>
-#include <string>
-
 #include <iostream>
+#include <string>
 
 namespace color_names
 {
@@ -91,8 +81,12 @@ std_msgs::msg::ColorRGBA makeColorMsg(std::string preset_name, double alpha)
 {
   std_msgs::msg::ColorRGBA c_msg;
   c_msg.a = alpha;
-  if (c_msg.a < 0.) {c_msg.a = 0.;}
-  if (c_msg.a > 1.) {c_msg.a = 1.;}
+  if (c_msg.a < 0.) {
+    c_msg.a = 0.;
+  }
+  if (c_msg.a > 1.) {
+    c_msg.a = 1.;
+  }
 
   auto found_itr = COLOR_NAME_DICT.find(preset_name);
   if (found_itr != COLOR_NAME_DICT.end()) {
